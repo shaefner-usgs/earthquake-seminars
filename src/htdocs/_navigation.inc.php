37a46fe455406fdcb29c2c86ea5b7a33d5ab28a0
Scott Haefner <shaefner@usgs.gov>
2016-08-05 15:13:11 -0700

    update text

--- src/htdocs/_navigation.inc.php
+++ src/htdocs/_navigation.inc.php
@@ -30,7 +30,7 @@ $NAVIGATION =
   navGroup('Seminars',
     navItem("$section", 'Upcoming', $matches) .
     $pastNav .
-    navItem("$section/committee.php", 'Seminar Committee') .
+    navItem("$section/committee.php", 'Committee') .
     navItem("http://online.wr.usgs.gov/kiosk/mparea3.html",
       'Campus Map and Directions')
   );
