99006b98030118e037d2b22b54c03eee25bc1d21
Scott Haefner <shaefner@usgs.gov>
2016-08-09 09:10:29 -0700

    add styles / hooks for seminar list

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -20,11 +20,45 @@ $db = new Db();
 // $year is NULL when viewing default page (upcoming seminars)
 $rsSeminars = $db->querySeminars($year);
 
-$seminarsHtml = '<ul>';
+$prevMonth = NULL;
 while ($row = $rsSeminars->fetch(PDO::FETCH_OBJ)) {
-  $seminarsHtml .= sprintf('<li>%s</li>',
-    $row->topic
+  $affiliation = NULL;
+  $timestamp = strtotime($row->datetime);
+  $seminarMonth = date('F', $timestamp);
+  $seminarYear = date('Y', $timestamp);
+
+  if ($seminarMonth !== $prevMonth) {
+    if ($prevMonth) {
+      $seminarsHtml .= '</ul>';
+    }
+    $seminarsHtml .= "<h2>$seminarMonth $seminarYear</h2>";
+    $seminarsHtml .= '<ul class="seminars no-style">';
+  }
+  if ($row->affiliation) {
+    $affiliation = ', ' . $row->affiliation;
+  }
+
+  $seminarsHtml .= sprintf('<li>
+      <a href="seminars/%s">
+        <div class="topic">
+          <h3>%s</h3>
+          <p>%s%s</p>
+        </div>
+        <time datetime="%s">
+          %s <span>%s</span>
+        </time>
+      </a>
+    </li>',
+    $row->ID,
+    $row->topic,
+    $row->speaker,
+    $affiliation,
+    date('c', $timestamp),
+    date('D, M j', $timestamp),
+    date('h:i A', $timestamp)
   );
+
+  $prevMonth = $seminarMonth;
 }
 $seminarsHtml .= '</ul>';
 
