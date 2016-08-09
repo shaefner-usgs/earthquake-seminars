f5fe36a0f6f976ced1352718bb2ce9666a6f8be5
Scott Haefner <shaefner@usgs.gov>
2016-08-09 11:12:23 -0700

    tweak display for edge cases

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -20,42 +20,69 @@ $db = new Db();
 // $year is NULL when viewing default page (upcoming seminars)
 $rsSeminars = $db->querySeminars($year);
 
+$cssClass = ' upcoming';
+if ($year) {
+  $cssClass = ' archives';
+}
+
 $prevMonth = NULL;
 while ($row = $rsSeminars->fetch(PDO::FETCH_OBJ)) {
   $affiliation = NULL;
   $timestamp = strtotime($row->datetime);
+
+  $seminarDate = date('D, M j', $timestamp);
+  $seminarDay = date('D', $timestamp);
   $seminarMonth = date('F', $timestamp);
+  $seminarTime = date('g:i A', $timestamp);
   $seminarYear = date('Y', $timestamp);
+
+  // Flag upcoming seminars that aren't on the "regular" day/time
+  if ($seminarDay !== 'Wed' && !$year) {
+    $seminarDate = "<mark>$seminarDate</mark>";
+  }
+  if ($seminarTime !== '10:30 AM' && !$year) {
+    $seminarTime = "<mark>$seminarTime</mark>";
+  }
 
   if ($seminarMonth !== $prevMonth) {
     if ($prevMonth) {
       $seminarsHtml .= '</ul>';
     }
     $seminarsHtml .= "<h2>$seminarMonth $seminarYear</h2>";
-    $seminarsHtml .= '<ul class="seminars no-style">';
+    $seminarsHtml .= '<ul class="seminars no-style' . $cssClass . '">';
   }
   if ($row->affiliation) {
     $affiliation = ', ' . $row->affiliation;
   }
 
+  // speaker field empty if no seminar (committee posts "no seminar" messages
+  if ($row->speaker) {
+    $openTag = '<a href="seminars/' . $row->ID . '">';
+    $closeTag = '</a>';
+  } else {
+    $openTag = '<div>';
+    $closeTag = '</div>';
+  }
+
   $seminarsHtml .= sprintf('<li>
-      <a href="seminars/%s">
+      %s
         <div class="topic">
           <h3>%s</h3>
           <p>%s%s</p>
         </div>
         <time datetime="%s">
-          %s <span>%s</span>
+          %s <span class="time">%s</span>
         </time>
-      </a>
+      %s
     </li>',
-    $row->ID,
+    $openTag,
     $row->topic,
     $row->speaker,
     $affiliation,
     date('c', $timestamp),
-    date('D, M j', $timestamp),
-    date('h:i A', $timestamp)
+    $seminarDate,
+    $seminarTime,
+    $closeTag
   );
 
   $prevMonth = $seminarMonth;
