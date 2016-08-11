ab77af865924837c7eed877f6abd3b76d9f3cbc8
Scott Haefner <shaefner@usgs.gov>
2016-08-11 16:04:44 -0700

    add info, update compatibility w/ model

--- src/lib/classes/SeminarView.class.php
+++ src/lib/classes/SeminarView.class.php
@@ -9,15 +9,15 @@
 class SeminarView {
   private $_model;
 
-  public function __construct ($model) {
+  public function __construct (Seminar $model) {
     $this->_model = $model;
   }
 
   private function _getVideoTag () {
-    return '<video src="' . $this->_model->video . '" width="704"
+    return '<video src="' . $this->_model->videoSrc . '" width="704"
       height="396" crossorigin="anonymous" controls="controls">
         <track label="English" kind="captions"
-          src="' . $this->_model->captions . '" default>
+          src="' . $this->_model->videoTrack . '" default="default">
     </video>';
   }
 
@@ -26,9 +26,14 @@ class SeminarView {
     if ($this->_model->summary) {
       print autop($this->_model->summary);
     }
-
-    $video = $this->_getVideoTag();
-
-    print $video;
+    print '<div class="row">';
+    print '  <div class="column three-of-four">';
+    print $this->_getVideoTag();
+    print '  </div>';
+    print '  <div class="column one-of-four">';
+    print '  <h4>' . $this->_model->speaker . '</h4>';
+    print '   <p>' . $this->_model->date . '</p>';
+    print '  </div>';
+    print '</div>';
   }
 }
