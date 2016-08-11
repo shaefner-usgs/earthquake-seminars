3b9bbbfb649f0e3611f3316bb50c260dbcc6cf55
Scott Haefner <shaefner@usgs.gov>
2016-08-11 16:03:42 -0700

    update model

--- src/lib/classes/Seminar.class.php
+++ src/lib/classes/Seminar.class.php
@@ -12,19 +12,31 @@ class Seminar {
     $datetime = $this->_data['datetime'];
     $timestamp = strtotime($datetime);
 
+    $this->_data['date'] = date('l, F j', $timestamp);
     $this->_data['dateShort'] = date('D, M j', $timestamp);
-    $this->_data['dateLong'] = date('l, F j', $timestamp);
+    $this->_data['day'] = date('l', $timestamp);
     $this->_data['month'] = date('F', $timestamp);
     $this->_data['time'] = date('g:i A', $timestamp);
+    $this->_data['timestamp'] = $timestamp;
     $this->_data['year'] = date('Y', $timestamp);
 
     $videoDomain = 'http://media.wr.usgs.gov';
     $videoPath = '/ehz/' . $this->_data['year'];
     $videoFile = str_replace('-', '', substr($datetime, 0, 10)) . '.mp4';
-    $captionsFile = str_replace('mp4', 'vtt', $videoFile);
 
-    $this->_data['video'] = $videoDomain . $videoPath . '/' . $videoFile;
-    $this->_data['captions'] = $videoDomain . $videoPath . '/' . $captionsFile;
+    $this->_data['videoSrc'] = $videoDomain . $videoPath . '/' . $videoFile;
+    $this->_data['videoTrack'] = str_replace('mp4', 'vtt', $this->_data['videoSrc']);
+
+    $this->_addAffiliation();
+  }
+
+  // add affiliation to speaker field
+  private function _addAffiliation () {
+    $speaker = $this->_data['speaker'];
+    if ($this->_data['affiliation']) {
+      $speaker .= ', ' . $this->_data['affiliation'];
+      $this->_data['speaker'] = $speaker;
+    }
   }
 
   public function __get ($name) {
