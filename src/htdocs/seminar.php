063fb6791c1cb97877ff6c08ff0a704260cfeac3
Scott Haefner <shaefner@usgs.gov>
2016-08-12 16:13:49 -0700

    add js, css

--- src/htdocs/seminar.php
+++ src/htdocs/seminar.php
@@ -12,8 +12,13 @@ $id = safeParam('id', '1056');
 if (!isset($TEMPLATE)) {
   $TITLE = 'Earthquake Science Center Seminars';
   $NAVIGATION = true;
-  $HEAD = '<link rel="stylesheet" href="'. $MOUNT_PATH . '/css/seminar.css" />';
-  $FOOT = '';
+  $HEAD = '
+    <link rel="stylesheet" href="lib/jwplayer-7.5.2/skins/five.css" />
+    <link rel="stylesheet" href="css/seminar.css" />';
+  $FOOT = '
+    <script src="lib/jwplayer-7.5.2/jwplayer.js"></script>
+    <script src="js/seminar.js"></script>
+  ';
 
   // Don't cache
   $expires = date(DATE_RFC2822);
