d95fc4244f6c62e612fa96792529d393f5888771
Scott Haefner <shaefner@usgs.gov>
2016-08-16 15:15:02 -0700

    refactor and include flags for type and period

--- src/lib/classes/Seminar.class.php
+++ src/lib/classes/Seminar.class.php
@@ -7,31 +7,18 @@
  */
 class Seminar {
   private $_data = [];
+  private $_seminarDate;
+  private $_todaysDate;
 
   public function __construct () {
-    // Add non-db fields to model
     if ($this->_data['datetime']) {
       $datetime = $this->_data['datetime'];
-      $timestamp = strtotime($datetime);
-      $year = date('Y', $timestamp);
 
-      $videoDomain = 'http://media.wr.usgs.gov';
-      $videoFile = str_replace('-', '', substr($datetime, 0, 10)) . '.mp4';
-      $videoPath = '/ehz/' . $year;
-
-      $this->_data['date'] = date('l, F j', $timestamp);
-      $this->_data['dateShort'] = date('D, M j', $timestamp);
-      $this->_data['day'] = date('l', $timestamp);
-      $this->_data['live'] = $this->_isLive($timestamp);
-      $this->_data['month'] = date('F', $timestamp);
-      $this->_data['time'] = date('g:i A', $timestamp);
-      $this->_data['timestamp'] = $timestamp;
-      $this->_data['type'] = $this->_getType($timestamp);
-      $this->_data['videoSrc'] = $videoDomain . $videoPath . '/' . $videoFile;
-      $this->_data['videoTrack'] = str_replace('mp4', 'vtt', $this->_data['videoSrc']);
-      $this->_data['year'] = $year;
+      $this->_seminarDate = date('Y-m-d', strtotime($datetime));
+      $this->_todaysDate = date('Y-m-d');
 
       $this->_addAffiliation();
+      $this->_addFields($datetime);
     }
   }
 
@@ -44,12 +31,48 @@ class Seminar {
     }
   }
 
-  private function _getType ($timestamp) {
-    $seminarDate = date('Y-m-d', $timestamp);
-    $todaysDate = date('Y-m-d');
-    $type = 'archive';
+  // Add non-db fields to model
+  private function _addFields ($datetime) {
+    $timestamp = strtotime($datetime);
+    $year = date('Y', $timestamp);
 
-    if ($seminarDate >= $todaysDate) {
+    $videoDomain = 'http://media.wr.usgs.gov';
+    $videoFile = str_replace('-', '', substr($datetime, 0, 10)) . '.mp4';
+    $videoPath = '/ehz/' . $year;
+    $videoSrc = $videoDomain . $videoPath . '/' . $videoFile;
+
+    $this->_data['date'] = date('F j, Y', $timestamp);
+    $this->_data['dateShort'] = date('D, M j', $timestamp);
+    $this->_data['day'] = date('l', $timestamp);
+    $this->_data['month'] = date('F', $timestamp);
+    $this->_data['period'] = $this->_getPeriod($timestamp);
+    $this->_data['time'] = date('g:i A', $timestamp);
+    $this->_data['timestamp'] = $timestamp;
+    $this->_data['type'] = $this->_getType();
+    $this->_data['videoSrc'] = $videoSrc;
+    $this->_data['videoTrack'] = str_replace('mp4', 'vtt', $videoSrc);
+    $this->_data['year'] = $year;
+  }
+
+  private function _getPeriod ($timestamp) {
+    $period = 'past'; // default value
+    if ($this->_seminarDate === $this->_todaysDate) {
+      if (time() < $timestamp) {
+        $period = 'today';
+      }
+      if ($this->_isLive($timestamp)) {
+        $period = 'live'; // "live" trumps "today" due to buffer
+      }
+    } else if ($this->_seminarDate > $this->_todaysDate) {
+      $period = 'future';
+    }
+
+    return $period;
+  }
+
+  private function _getType () {
+    $type = 'archive'; // default value
+    if ($this->_seminarDate >= $this->_todaysDate) {
       $type = 'upcoming';
     }
 
