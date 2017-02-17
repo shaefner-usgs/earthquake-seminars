7a5f04a294722a529490634bf37dacb2330d3a4c
Scott Haefner <shaefner@usgs.gov>
2017-02-17 09:13:51 -0800

    request mp4, vtt files over https

--- src/lib/classes/Seminar.class.php
+++ src/lib/classes/Seminar.class.php
@@ -36,7 +36,7 @@ class Seminar {
     $timestamp = strtotime($datetime);
     $year = date('Y', $timestamp);
 
-    $videoDomain = 'http://media.wr.usgs.gov';
+    $videoDomain = 'https://media.wr.usgs.gov';
     $videoFile = str_replace('-', '', substr($datetime, 0, 10)) . '.mp4';
     $videoPath = '/ehz/' . $year;
     $videoSrc = $videoDomain . $videoPath . '/' . $videoFile;
