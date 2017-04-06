90c27fa81c3340ce3a0b9f2b4227d93ae1dae067
Scott Haefner <shaefner@usgs.gov>
2017-04-06 14:49:46 -0700

    add styling hook

--- src/htdocs/_feeds.inc.php
+++ src/htdocs/_feeds.inc.php
@@ -3,7 +3,7 @@
 include_once '../conf/config.inc.php'; // app config
 
 $feeds = '
-<h3>Video Podcast</h3>
+<h3 class="podcast">Video Podcast</h3>
 <ul class="feeds no-style">
   <li class="itunes">
     <a href="http://itunes.apple.com/us/podcast/usgs-earthquake-science-center/id413770595">
