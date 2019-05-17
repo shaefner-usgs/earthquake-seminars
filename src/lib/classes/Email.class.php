51bc01ac46e0a36c5f1830921affe646ae88c25b
Scott Haefner <shaefner@usgs.gov>
2019-05-17 15:29:36 -0700

    initial commit

---
+++ src/lib/classes/Email.class.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Create and send an email
+ *
+ * @param $options {Array}
+ *   [
+ *      'data': {Array} // key-value pairs for populating email template
+ *      'from': {String}
+ *      'template': {String} // full path to template file
+ *      'subject': {String}
+ *      'to': {String}
+ *   ]
+ */
+class Email {
+  private $_data, $_from, $_message, $_template, $_subject, $_to;
+
+  public function __construct($options) {
+    $this->_data = $options['data'];
+    $this->_from = $options['from'];
+    $this->_message = $this->_getTemplate();
+    $this->_template = $options['template'];
+    $this->_subject = $options['subject'];
+    $this->_to = $options['to'];
+
+    $this->_create(); // Create the message
+  }
+
+  /**
+   * Create email message body
+   */
+  private function _create() {
+    // Substitute seminar data for mustache placeholders
+    foreach ($this->_data as $key => $value) {
+      $pattern = "/\{\{$key\}\}/";
+      $this->_message = preg_replace($pattern, $value, $template);
+    }
+  }
+
+  /**
+   *  Read email template into a string and return it
+   *
+   * @return {String}
+   */
+  private function _getTemplate() {
+    return file_get_contents($this->_template);
+  }
+
+  /**
+   * Send email
+   */
+  public function send() {
+    $headers = [
+      'From: ' . $this->_from,
+      'MIME-Version: 1.0',
+      'Content-type: text/html; charset=iso-8859-1'
+    ];
+
+    mail($this->_to, $this->_subject, $this->_message, implode('\r\n', $headers));
+  }
+}
