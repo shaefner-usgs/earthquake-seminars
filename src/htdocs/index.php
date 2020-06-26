1bcc139a5a2ffaf0355c9dc12719463b381bc3ed
Scott Haefner <shaefner@usgs.gov>
2020-06-26 09:32:18 -0700

    tweak title tag; show seminar venue details on archives too

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -14,6 +14,9 @@ $year = safeParam('year'); // gets set if user viewing archives
 
 if (!isset($TEMPLATE)) {
   $TITLE = 'Earthquake Science Center Seminars';
+  if ($year) {
+    $TITLETAG = 'Archives | ' . $TITLE;
+  }
   $NAVIGATION = true;
   $HEAD = '<link rel="stylesheet" href="'. $MOUNT_PATH . '/css/index.css" />';
   $FOOT = '';
@@ -41,23 +44,25 @@ foreach($seminars as $seminar) {
 
 $view = new SeminarListView($seminarCollection);
 
-if (!$year) { // show on Upcoming tab only ?>
+?>
 
 <div class="row details">
-  <div class="column one-of-five"><img src="seminars/img/podcast-small.png" alt="podcast icon" /></div>
+  <div class="column one-of-five">
+    <img src="<?php print $MOUNT_PATH; ?>/img/podcast-small.png" alt="podcast icon" />
+  </div>
   <div class="column four-of-five">
     <p>Seminars typically take place at <strong>10:30 AM</strong> on
-      <strong>Wednesdays</strong> in the <strong>Yosemite Conference Room</strong> (Rm 2030A, Bldg 19). The USGS Campus is located at
+      <strong>Wednesdays</strong> in the <strong>Yosemite Conference
+      Room</strong> (Rm 2030A, Bldg 19). The USGS Campus is located at
       <a href="/contactus/menlo/menloloc.php" title="Campus Map and
       Directions">350 North Akron Road, Moffett Field, CA</a>.</p>
-
     <p>We record most seminars. You can watch live or
       <a href="<?php print $MOUNT_PATH; ?>/archives/<?php print $currentYear; ?>">check
       the archives</a> to view a past seminar.</p>
   </div>
 </div>
 
-<?php }
+<?php
 
 $view->render();
 
