f78c6d02f3ad8c8cd4bd44653104c83e14d794c7
Scott Haefner <4175182+shaefner-usgs@users.noreply.github.com>
2021-04-21 22:17:26 +0000

    comment out test email

--- src/lib/email/email.php
+++ src/lib/email/email.php
@@ -27,7 +27,7 @@ prepare('+2 days', $USGS_EMAIL);
 prepare('+7 days', $NASA_EMAIL);
 
 // Test announcement
- prepare('2021-04-28 10:30:00', $ADMIN_EMAIL);
+// prepare('2021-04-28 10:30:00', $ADMIN_EMAIL);
 
 
 /**
